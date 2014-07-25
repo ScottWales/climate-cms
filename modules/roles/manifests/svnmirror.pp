ccbfadba2d4d9730cb92004f295ccc3aba046d1a
Scott Wales <scott.wales@unimelb.edu.au>
2014-07-25 15:15:47 +1000

    More array levels

--- modules/roles/manifests/svnmirror.pp
+++ modules/roles/manifests/svnmirror.pp
@@ -57,12 +57,12 @@ class roles::svnmirror (
   apacheplus::vhost {"${vhost}-redirect":
     port     => 80,
     docroot  => '/var/www/null',
-    rewrites =>
-    {
-      comment      => 'SSL',
-      rewrite_cond => ['%{HTTPS} != on'],
-      rewrite_rule => ['^(.*)$ https://%{HTTP_HOST}/$1 [R=301,L]'],
-    }
+    rewrites =>[
+      {
+        comment      => 'SSL',
+        rewrite_cond => ['%{HTTPS} != on'],
+        rewrite_rule => ['^(.*)$ https://%{HTTP_HOST}/$1 [R=301,L]'],
+      }]
   }
   apacheplus::vhost {$vhost:
     ssl     => true,
