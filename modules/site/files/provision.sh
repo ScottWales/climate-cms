85bc2753adb744c7c972b6b063606773e094accd
Scott Wales <scott.wales@unimelb.edu.au>
2014-07-25 12:28:46 +1000

    Fix for librarian

--- modules/site/files/provision.sh
+++ modules/site/files/provision.sh
@@ -16,6 +16,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+export HOME=/home/ec2-user
 cd /etc/puppet/environments/production
 git pull
 librarian-puppet install --verbose
