8f27f67fb91955f729cbfe297a9a25ade1262666
Scott Wales <scott.wales@unimelb.edu.au>
2014-07-28 16:22:59 +1000

    Add firewall rules

---
+++ modules/site/manifests/firewall/pre.pp
@@ -0,0 +1,40 @@
+## \file    modules/site/manifests/firewall/pre.pp
+#  \author  Scott Wales <scott.wales@unimelb.edu.au>
+#
+#  Copyright 2014 ARC Centre of Excellence for Climate Systems Science
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+class site::firewall::pre {
+
+  Firewall {
+    require => undef,
+  }
+
+  # Default firewall rules
+  firewall { '000 accept all icmp':
+    proto   => 'icmp',
+    action  => 'accept',
+  }->
+  firewall { '001 accept all to lo interface':
+    proto   => 'all',
+    iniface => 'lo',
+    action  => 'accept',
+  }->
+  firewall { '002 accept related established rules':
+    proto  => 'all',
+    state  => ['RELATED', 'ESTABLISHED'],
+    action => 'accept',
+  }
+
+}
