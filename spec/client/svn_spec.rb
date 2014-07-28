47d5067a67d412ad2e0242c72310bd0614c3618d
Scott Wales <scott.wales@unimelb.edu.au>
2014-07-28 12:44:53 +1000

    Add ips

--- spec/client/svn_spec.rb
+++ spec/client/svn_spec.rb
@@ -5,6 +5,8 @@ RSpec.configure do |c|
     c.disable_sudo = true
 end
 
+server = '130.56.244.76'
+
 describe 'Check svn client version' do
     describe command('svn --version') do
         it {should return_exit_status 0}
@@ -16,16 +18,28 @@ describe 'Check fcm client version' do
     end
 end
 
-describe 'Check connection to mirror repo' do
-    repo = 'https://svn.accessdev.nci.org.au/ext_um/UM'
-    describe command("svn info --non-interactive #{repo}") do
-        it {should return_exit_status 0}
+describe 'Client svn access' do
+    describe 'Check connection to mirror repo' do
+        repo = "https://#{server}/ext_um/UM"
+        describe command("svn info --non-interactive #{repo}") do
+            it {should return_exit_status 0}
+        end
     end
-end
 
-describe 'Check FCM alias' do
-    repo = 'fcm:ext/UM'
-    describe command("fcm info --non-interactive #{repo}") do
-        it {should return_exit_status 0}
+    describe 'Check FCM alias' do
+        repo = 'fcm:ext/UM'
+        describe command("fcm info --non-interactive #{repo}") do
+            it {should return_exit_status 0}
+        end
+    end
+
+    describe 'Check connection to sync repo' do
+        repo = "https://#{server}/um_ext-sync"
+        describe command("svn info --non-interactive --trust-server-cert #{repo}") do
+            it {should_not return_exit_status 0}
+        end
+        describe command("curl -skL --write-out %{http_code}     --output /dev/null #{repo}") do
+            it { should return_stdout '403' }
+        end
     end
 end
