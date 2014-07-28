a9b08e16e061ad7521a881e25bd9d4702b3a781f
Scott Wales <scott.wales@unimelb.edu.au>
2014-07-28 12:05:31 +1000

    Test sync point

--- spec/server/svnserve_spec.rb
+++ spec/server/svnserve_spec.rb
@@ -8,3 +8,12 @@ describe "Local svn access" do
         end
     end
 end
+
+describe "Sync access" do
+    describe 'Check connection to repo' do
+        repo = 'https://svn/um_ext-sync'
+        describe command("svn info --non-interactive --trust-server-cert #{repo}") do
+            it {should return_exit_status 0}
+        end
+    end
+end
