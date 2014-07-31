ba4736224ffa12f6790c5cd38865717f6ea677dd
Scott Wales <scott.wales@unimelb.edu.au>
2014-07-31 14:36:56 +1000

    Typo

--- modules/roles/manifests/svnmirror.pp
+++ modules/roles/manifests/svnmirror.pp
@@ -128,7 +128,8 @@ class roles::svnmirror (
   }
 
   supervisord::program {'svnsync-listener':
-    command => '/usr/local/bin/python /usr/local/bin/svnsync-listener.py',
+    command => '/usr/bin/python /usr/local/bin/svnsync-listener.py',
     user    => $user,
+    require => File['puppet:///modules/roles/svnmirror/update-service.py'],
   }
 }
